85088a7205cc997daa290b7f8b118d602ef8e684
张瑞成 <zhangruicheng@yntzone.com>
2024-08-07 18:05:57 +0800

    Finish Learning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://voluble-creponne-18077c.netlify.app",
+  site: "https://voluble-creponne-18077c.netlify.app",
+  integrations: [preact()]
 });
